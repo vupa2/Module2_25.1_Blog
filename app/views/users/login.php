16794403c2a6bb518daa65ef4bafee82191c7fb8
vupa <you@example.com>
2021-05-29 15:13:08 +0700

    update

--- app/views/users/login.php
+++ app/views/users/login.php
@@ -10,10 +10,10 @@
         <h1 class="h2 mb-3 fw-normal text-uppercase text fw-bold">Đăng Nhập</h1>
 
         <div class="form-floating mb-2">
-            <?php if (!empty($data['usernameError'])) : ?>
-                <input type="text" class="form-control is-invalid" name="username" id="floating-username" placeholder="Tên đăng nhập">
+            <?php if (!empty($errors['username'])) : ?>
+                <input type="text" class="form-control is-invalid" name="username" id="floating-username" placeholder="Tên đăng nhập" value="<?= $data['username'] ?>">
                 <div class="invalid-feedback">
-                    <?= $data['usernameError'] ?>
+                    <?= $errors['username'] ?>
                 </div>
             <?php else : ?>
                 <input type="text" class="form-control" name="username" id="floating-username" placeholder="Tên đăng nhập">
@@ -22,18 +22,18 @@
         </div>
 
         <div class="form-floating mb-3">
-            <?php if (!empty($data['passwordError'])) : ?>
+            <?php if (!empty($errors['password'])) : ?>
                 <input type="password" class="form-control is-invalid" name="password" id="floating-password" placeholder="Mật khẩu">
                 <div class="invalid-feedback">
-                    <?= $data['passwordError'] ?>
+                    <?= $errors['password'] ?>
                 </div>
             <?php else : ?>
                 <input type="password" class="form-control" name="password" id="floating-password" placeholder="Mật khẩu">
             <?php endif; ?>
 
-            <?php if (!empty($data['allError'])) : ?>
+            <?php if (!empty($errors['all'])) : ?>
                 <div class="is-invalid text-danger mt-1 text-center">
-                    <?= $data['allError'] ?>
+                    <?= $errors['all'] ?>
                 </div>
             <?php endif; ?>
 
